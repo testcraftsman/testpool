5acecba18089dfce4d1d55b396f96868b38ce739
Mark Hamilton <mark_lee_hamilton@att.net>
2018-09-20 15:26:19 -0700

    WIP

--- testpool/db/djconfig/settings/components/base.py
+++ testpool/db/djconfig/settings/components/base.py
@@ -119,10 +119,8 @@ USE_L10N = True
 
 ##
 USE_TZ = False
-#  USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
